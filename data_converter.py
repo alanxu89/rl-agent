b826d77404db1df8f6d8a8e71b2e4cb46df32a08
Xunnong Xu <xunnongxu@foxmail.com>
2022-05-14 22:50:02 +0800

    waymo data converter

---
+++ data_converter.py
@@ -0,0 +1,154 @@
+from abc import ABC, abstractmethod
+import os
+import time
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from waymodataset.scenario_pb2 import Scenario
+
+
+class DataConverter:
+    """ convert motion data to sim env data
+    """
+
+    @abstractmethod
+    def read(self, data_file):
+        pass
+
+    @abstractmethod
+    def get_a_scenario(self):
+        pass
+
+
+class WaymoDataConverter(DataConverter):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.dataset = None
+        self.num_records = 0
+        self.record_idx = -1
+        self.mph_to_mps = 1609.0 / 3600
+
+    def read(self, data_file):
+        self.dataset = list(tf.data.TFRecordDataset([data_file]))
+        self.num_records = len(self.dataset)
+
+    def get_a_scenario(self, idx=None, mode='sequential'):
+        """ get scenario by idx or sequential or random
+        """
+        if idx is not None:
+            self.record_idx = idx
+        elif mode == "random":
+            self.record_idx = random.randint(0, self.num_records - 1)
+        else:
+            self.record_idx += 1
+        raw_record = self.dataset[self.record_idx]
+        scenario = self.__parse_record(raw_record.numpy())
+
+        ts = scenario.timestamps_seconds
+
+        tracks = []
+        for track in scenario.tracks:
+            track_data = []
+            for state in track.states:
+                track_data.append([
+                    state.center_x, state.center_y, state.center_z,
+                    state.length, state.width, state.height, state.heading,
+                    state.velocity_x, state.velocity_y, state.valid
+                ])
+            track_data = np.array(track_data)
+            tracks.append(track_data)
+        tracks = np.array(tracks)
+
+        lanes = []
+        lanes_speed_limit = []
+        for map_feature in scenario.map_features:
+            if map_feature.HasField('lane'):
+                lane_data = []
+                for point in map_feature.lane.polyline:
+                    lane_data.append([
+                        point.x,
+                        point.y,
+                        point.z,
+                    ])
+                lane_data = np.array(lane_data)
+                lanes.append(lane_data)
+                lanes_speed_limit.append(map_feature.lane.speed_limit_mph *
+                                         self.mph_to_mps)
+
+        road_edges = []
+        for map_feature in scenario.map_features:
+            if map_feature.HasField('road_edge'):
+                lane_data = []
+                for point in map_feature.road_edge.polyline:
+                    lane_data.append([
+                        point.x,
+                        point.y,
+                        point.z,
+                    ])
+                lane_data = np.array(lane_data)
+                road_edges.append(lane_data)
+
+        road_lines = []
+        road_lines_type = []
+        for map_feature in scenario.map_features:
+            if map_feature.HasField('road_line'):
+                lane_data = []
+                for point in map_feature.road_line.polyline:
+                    lane_data.append([
+                        point.x,
+                        point.y,
+                        point.z,
+                    ])
+                lane_data = np.array(lane_data)
+                road_lines.append(lane_data)
+                road_lines_type.append(map_feature.road_line.type)
+
+        dynamics_map_states = []
+        for dms in scenario.dynamic_map_states:
+            dynamic_states = []
+            for lane_state in dms.lane_states:
+                dynamic_state = {}
+                dynamic_state['lane'] = lane_state.lane
+                dynamic_state['state'] = lane_state.state
+                dynamic_state['stop_point'] = np.array([
+                    lane_state.stop_point.x, lane_state.stop_point.y,
+                    lane_state.stop_point.z
+                ])
+                dynamic_states.append(dynamic_state)
+            dynamics_map_states.append(dynamic_states)
+
+        scenario_data = {
+            'scenario_id': scenario.scenario_id,
+            'timestamps': ts,
+            'tracks': tracks,
+            'lanes': lanes,
+            'lanes_speed_limit': lanes_speed_limit,
+            'road_edges': road_edges,
+            'road_lines': road_lines,
+            'road_lines_type': road_lines_type,
+            'dynamics_map_states': dynamics_map_states
+        }
+
+        return scenario_data
+
+    def __parse_record(self, raw_record):
+        example = Scenario()
+        example.ParseFromString(raw_record)
+        return example
+
+
+if __name__ == "__main__":
+    waymo = WaymoDataConverter()
+    data_dir = "/home/alanxu/Downloads/waymo_motion_data"
+    fname = "uncompressed_scenario_training_training.tfrecord-00491-of-01000"
+    waymo.read(os.path.join(data_dir, fname))
+    t0 = time.time()
+    for _ in range(10):
+        s = waymo.get_a_scenario(idx=0)
+        print(s['scenario_id'])
+    print(time.time() - t0)
+    time.sleep(5)
